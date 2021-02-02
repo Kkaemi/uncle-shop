2ba6da84edc6e0a71724d1f10faae83a75ea5d1e
SeongHyeon Ji <kitys2k3@naver.com>
2021-02-02 17:54:15 +0900

    Spring Boot 프로젝트 생성

---
+++ src/main/java/com/uncleshop/uncleshop/UncleShopApplication.java
@@ -0,0 +1,13 @@
+package com.uncleshop.uncleshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncleShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncleShopApplication.class, args);
+	}
+
+}
