472081f38ce9a527a7ea46e68911be7b240bfbb8
YangChoi <yang.hj.r@gmail.com>
2021-02-06 17:52:36 +0900

    test commit
    test commit

--- src/main/java/com/uncleshop/uncleshop/UncleShopApplication.java
+++ src/main/java/com/uncleshop/uncleshop/UncleShopApplication.java
@@ -3,6 +3,7 @@ package com.uncleshop.uncleshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test commit 
 @SpringBootApplication
 public class UncleShopApplication {
 
